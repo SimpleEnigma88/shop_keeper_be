3a2ac02f7c895881ced64ca6d0db61ce91482917
Andy Moore <andymoore88@hotmail.com>
2024-03-16 14:16:04 -0500

    Refactor CharacterMagicItemsController

--- app/controllers/character_magic_items_controller.rb
+++ app/controllers/character_magic_items_controller.rb
@@ -1,31 +1,30 @@
 class CharacterMagicItemsController < ApplicationController
   before_action :set_character
 
-    def index
-        if @character.magic_items
-            @character_magic_items = @character.magic_items
-            render json: @character_magic_items, status: :ok
-        else
-            render json: @character.errors, status: :unprocessable_entity
-        end
+  def index
+    if @character.magic_items
+      @character_magic_items = @character.magic_items
+      render json: @character_magic_items, status: :ok
+    else
+      render json: @character.errors, status: :unprocessable_entity
     end
+  end
 
-    def show
-        if @character.magic_items
-            @character_magic_item = @character.magic_items.find(params[:id])
-            render json: @character_magic_item, status: :ok
-        else 
-            render json: @character.errors, status: :unprocessable_entity
-        end
+  def show
+    if @character.magic_items
+      @character_magic_item = @character.magic_items.find(params[:id])
+      render json: @character_magic_item, status: :ok
+    else
+      render json: @character.errors, status: :unprocessable_entity
     end
-
+  end
 
   def create
     magic_item = MagicItem.find(params[:magic_item_id])
     @character.magic_items << magic_item
 
     if @character.save
-          render json: magic_item, status: :created
+      render json: magic_item, status: :created
     else
       render json: @character.errors, status: :unprocessable_entity
     end
