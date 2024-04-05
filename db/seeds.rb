14ea142edf547a77f697b0a0fcee7dec768ee8b9
Andy Moore <andymoore88@hotmail.com>
2024-04-05 17:17:35 -0500

    Refactor seeds.rb and magic_items_seed.rb for improved readability and maintainability

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,82 +9,85 @@
 #   end
 # frozen_string_literal: true
 
-require 'faker'
-require 'json'
+# require 'faker'
+# require 'json'
 
-path = Rails.root.join('db', 'seeds', 'ModifiedMagicItems.json')
-records = JSON.parse(File.read(path))
-MagicItem.destroy_all
+# db/seeds.rb
+load(Rails.root.join('db', 'seeds', 'magic_items_seed.rb'))
 
-ActiveRecord::Base.transaction do
-  records.each do |record_data|
-    filtered_data = {
-      name: record_data['name'],
-      category: record_data['type'],
-      desc: record_data['desc'],
-      rarity: record_data['rarity'],
-      requires_attunement: record_data['requires_attunement']
-    }
-    puts filtered_data
+# path = Rails.root.join('db', 'seeds', 'magic_items_seed.rb')
+# records = JSON.parse(File.read(path))
+# MagicItem.destroy_all
 
-    begin
-      MagicItem.create!(filtered_data)
-    rescue ActiveRecord::RecordInvalid => e
-      puts "An error occurred while creating the record: #{e.message}"
-      # Optionally log the record that failed to aid debugging
-      puts "Failed record data: #{filtered_data.inspect}"
-      raise ActiveRecord::Rollback
-    end
-  end
-end
+# ActiveRecord::Base.transaction do
+#   records.each do |record_data|
+#     filtered_data = {
+#       name: record_data['name'],
+#       category: record_data['type'],
+#       desc: record_data['desc'],
+#       rarity: record_data['rarity'],
+#       requires_attunement: record_data['requires_attunement']
+#     }
+#     puts filtered_data
+
+#     begin
+#       MagicItem.create!(filtered_data)
+#     rescue ActiveRecord::RecordInvalid => e
+#       puts "An error occurred while creating the record: #{e.message}"
+#       # Optionally log the record that failed to aid debugging
+#       puts "Failed record data: #{filtered_data.inspect}"
+#       raise ActiveRecord::Rollback
+#     end
+#   end
+# end
 
 # Seed Players
-10.times do
-  Player.create!(
-    user_name: Faker::Internet.username,
-    email: Faker::Internet.email,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
-  )
-end
+# 10.times do
+#   Player.create!(
+#     user_name: Faker::Internet.username,
+#     email: Faker::Internet.email,
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name
+#   )
+# end
 
-Player.all.each do |player|
-  rand(1..3).times do
-    Character.create!(
-      name: Faker::Games::DnD.first_name,
-      char_class: Faker::Games::DnD.klass,
-      level: rand(1..20),
-      player:
-    )
-  rescue ActiveRecord::RecordInvalid => e
-    puts "Failed to create character for player #{player.id}: #{e.message}"
-  end
-end
+# Player.all.each do |player|
+#   rand(1..3).times do
+#     Character.create!(
+#       name: Faker::Games::DnD.first_name,
+#       char_class: Faker::Games::DnD.klass,
+#       level: rand(1..20),
+#       player:
+#     )
+#   rescue ActiveRecord::RecordInvalid => e
+#     puts "Failed to create character for player #{player.id}: #{e.message}"
+#   end
+# end
 
-players = Player.all.shuffle
+# players = Player.all.shuffle
 
-players.each_with_index do |player, index|
-  # Only create a party for 1 out of every 1-3 players
-  next unless (index % rand(1..3)).zero?
+# players.each_with_index do |player, index|
+#   # Only create a party for 1 out of every 1-3 players
+#   next unless (index % rand(1..3)).zero?
 
-  party = Party.create!(
-    name: Faker::Games::DnD.city,
-    dm_player: player
-  )
+#   party = Party.create!(
+#     name: Faker::Games::DnD.city,
+#     dm_player: player
+#   )
 
-  # Select characters from different players
-  characters = players.sample(rand(2..5)).map do |p|
-    p.characters.sample
-  end
+#   # Select characters from different players
+#   characters = players.sample(rand(2..5)).map do |p|
+#     p.characters.sample
+#   end
 
-  # 35% of the time, add the DM's character to the party
-  characters << player.characters.sample if rand < 0.35 && player.characters.any?
+#   # 35% of the time, add the DM's character to the party
+#   characters << player.characters.sample if rand < 0.35 && player.characters.any?
 
-  characters.each do |character|
-    character.update!(party:)
+#   characters.each do |character|
+#     character.update!(party:)
 
-    # Assign a random number of magic items from the magic_items table to each character
-    magic_items = MagicItem.order('RANDOM()').limit(rand(1..5))
-    character.magic_items << magic_items
-  end
-end
+#     # Assign a random number of magic items from the magic_items table to each character
+#     magic_items = MagicItem.order('RANDOM()').limit(rand(1..5))
+#     character.magic_items << magic_items
+#   end
+# end
