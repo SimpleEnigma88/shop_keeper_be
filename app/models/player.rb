d1e5c29ecdffbd19a6b8cde7b5523dc29ef0c44f
Robert Andrew Moore <ra.moore573@gmail.com>
2024-04-13 19:41:41 -0500

    Update validation error messages for name uniqueness in Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,8 +5,8 @@ class Player < ApplicationRecord
 
   has_many :characters
 
-  validates :user_name, presence: true, uniqueness: true, message: 'That user name has already been taken.'
-  validates :email, presence: true, uniqueness: true, message: 'That email has already been registered.'
+  validates :user_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true
