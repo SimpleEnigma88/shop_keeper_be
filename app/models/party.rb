c67325850fb69be65fd6667df9f640f5a1b983ca
Robert Andrew Moore <ra.moore573@gmail.com>
2024-04-13 19:37:42 -0500

    Update validation error messages for name uniqueness in Character, Party, and Player models

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,6 +2,6 @@ class Party < ApplicationRecord
   belongs_to :dm_player, class_name: 'Player', foreign_key: 'dm_player_id', optional: false
 
   has_many :characters
-  validates :name, presence: true, uniqueness: { scope: :dm_player_id, message: 'has already been taken' }
+  validates :name, presence: true, uniqueness: { scope: :dm_player_id, message: 'That name has already been taken' }
   validates :dm_player_id, presence: true
 end
